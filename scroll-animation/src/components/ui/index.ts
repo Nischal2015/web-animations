d4090e9b78d5ced34962864bdb1269ac3f03b001
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-31 13:09:57 +0545

    refactor: add components
    - add star rating component to show stars
    - add avatar component with name and country
    - add card component

--- scroll-animation/src/components/ui/index.ts
+++ scroll-animation/src/components/ui/index.ts
@@ -1,3 +1,8 @@
+export { default as Avatar } from "./avatar";
+export * from "./avatar";
+
 export { Button } from "./button";
 
 export { Link } from "./link";
+
+export { default as StarRating } from "./star-rating";
