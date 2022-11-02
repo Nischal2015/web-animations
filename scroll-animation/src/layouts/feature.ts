5b6593a0e0d443e419d9acf4d02f08c7830be42f
Nischal Shakya <nischalshakya2015@gmail.com>
2022-11-02 12:41:34 +0545

    styles: finish layout styles

--- scroll-animation/src/layouts/feature.ts
+++ scroll-animation/src/layouts/feature.ts
@@ -6,9 +6,18 @@ type FeatureContentProps = {
   featureMd?: boolean;
 };
 
+type FeatureParticleProps = {
+  width: number;
+  height: number;
+  top?: number;
+  left?: number;
+  right?: number;
+  bottom?: number;
+};
+
 export const FeatureContent = styled.div<FeatureContentProps>(
   {
-    padding: "13rem 0 12.1rem",
+    padding: "17.5rem 0 18rem",
   },
   (props) => {
     if (props.featureLg) {
@@ -17,19 +26,20 @@ export const FeatureContent = styled.div<FeatureContentProps>(
       };
     } else if (props.featureMd) {
       return {
-        padding: "17.5rem 0 18rem",
+        padding: "13rem 0 12.1rem",
       };
     }
   }
 );
 
-export const FeatureBody = styled.div({
+export const FeatureBody = styled(motion.div)({
   maxWidth: "42rem",
 });
 
-export const FeatureGrid = styled.div({
+export const FeatureGrid = styled(motion.div)({
   display: "grid",
   gridTemplateColumns: "repeat(2, minmax(100px, 1fr))",
+  alignItems: "center",
 });
 
 export const FeatureTitle = styled(motion.div)({
@@ -37,7 +47,7 @@ export const FeatureTitle = styled(motion.div)({
   "& > h2": {
     fontSize: "5.6rem",
     lineHeight: 1.1,
-    letterSpacing: "-0.5px",
+    letterSpacing: "-1.5px",
     fontWeight: 700,
     whiteSpace: "nowrap",
   },
@@ -58,14 +68,15 @@ export const ImageDiv = styled.div({
   justifyContent: "center",
 });
 
-export const FeatureImage = styled.div({
-  position: "relative",
+export const FeatureImage = styled(motion.div)({
   width: "23.9rem",
   height: "50.8rem",
   margin: "0 auto",
+  position: "relative",
+  zIndex: 1,
 });
 
-export const FeatureImageSpan = styled.span<{ url: string }>(
+export const FeatureImageSpan = styled(motion.span)<{ url: string }>(
   {
     display: "inline-block",
     width: "100%",
@@ -78,9 +89,44 @@ export const FeatureImageSpan = styled.span<{ url: string }>(
 
 export const FeatureImageParticle = styled.div({
   position: "absolute",
-  background: "#ff000055",
   top: 0,
   left: 0,
   bottom: 0,
   right: 0,
+  zIndex: -1,
 });
+
+export const FeatureParticle = styled(motion.div)<FeatureParticleProps>(
+  (props) => {
+    const cssObject = {
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    } as FeatureParticleProps;
+
+    const cssCompatibleKeys = Object.keys(props).filter(
+      (key) => key in cssObject
+    );
+    const filteredKeys = cssCompatibleKeys.filter(
+      (k) => props[k as keyof typeof props]
+    );
+    const filteredObject = filteredKeys.reduce(
+      (a, v) => ({ ...a, [v]: `${props[v as keyof typeof props]}px` }),
+      {}
+    );
+
+    return {
+      position: "absolute",
+      width: `${props.width}rem`,
+      height: `${props.height}rem`,
+      ...filteredObject,
+      "& img": {
+        width: "100%",
+        height: "100%",
+      },
+    };
+  }
+);
